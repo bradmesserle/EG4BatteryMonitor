a5b69c484fff806e0275c6a908587090dc63b3b1
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-09-20 23:39:20 -0400

    Updated page with the battery status stream

--- internal/eg4/battery-info.go
+++ internal/eg4/battery-info.go
@@ -48,10 +48,10 @@ func run(frames <-chan serialcan.Frame, interval time.Duration) {
 // publish sends summarized data to the front end. Posting the data is done via the internal.EventBus.
 func publish(row Row) {
 
-	out := struct {
-		Timestamp string `json:"timestamp"`
-		Row
-	}{Timestamp: time.Now().Format("2006-01-02T15:04:05"), Row: row}
+	//out := struct {
+	//	Timestamp string `json:"timestamp"`
+	//	Row
+	//}{Timestamp: time.Now().Format("2006-01-02T15:04:05"), Row: row}
 	//b, _ := json.Marshal(out)
 	//fmt.Println(string(b))
 
