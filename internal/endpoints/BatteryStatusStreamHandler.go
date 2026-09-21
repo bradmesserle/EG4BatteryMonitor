25685985379cbd054a776255cd0c0aa5de80cbb9
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-09-20 22:57:16 -0400

    Updated page with the battery status stream

--- internal/endpoints/BatteryStatusStreamHandler.go
+++ internal/endpoints/BatteryStatusStreamHandler.go
@@ -40,6 +40,7 @@ func BatteryStatusStreamHandler(c *echo.Context) error {
                        const data = %s;
                        console.log(data);
                        console.log(data.soc_pct);
+                       set(data.soc_pct)
              `, msg)
 
 			err := sse.ExecuteScript(script)
